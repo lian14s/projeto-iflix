85cd3dd0d8c4e6006639964595ad1599e2dd0250
Lian Lago <lianlago392@gmail.com>
2026-09-23 19:44:11 -0300

    projeto iniciado

---
+++ iflix/src/test/java/br/edu/iffar/iflix/IflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iffar.iflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
